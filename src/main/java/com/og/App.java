486ffa41367276f8f11f23bbbe554bfbc75a74b6
Rich <rich_kacor@hotmail.com>
2019-04-13 15:05:15 -0500

    added text 2

--- src/main/java/com/og/App.java
+++ src/main/java/com/og/App.java
@@ -13,6 +13,6 @@ public class App
     public static void main( String[] args )
     {
         out.println(StringEscapeUtils.escapeHtml4("here is a <p> tag. Added some more text"));
-        out.println("rkacor2");
+        out.println("rkacor32");
     }
 }
