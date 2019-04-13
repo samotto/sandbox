8ae7860ee80d6c975693e3569cb9aa2e1f3a584d
Rich <rich_kacor@hotmail.com>
2019-04-13 15:03:26 -0500

    third change

--- src/main/java/com/og/App.java
+++ src/main/java/com/og/App.java
@@ -2,6 +2,8 @@ package com.og;
 
 import org.apache.commons.lang3.StringEscapeUtils;
 
+import static java.lang.System.*;
+
 /**
  * Hello world!
  *
@@ -10,7 +12,7 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println(StringEscapeUtils.escapeHtml4("here is a <p> tag. Added some more text"));
-        System.out.println("rkacor");
+        out.println(StringEscapeUtils.escapeHtml4("here is a <p> tag. Added some more text"));
+        out.println("rkacor2");
     }
 }
